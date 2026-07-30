08584c94aa991f00dc1d07d74b9e7d1fa0719294
Sasank Yadavalli <yadavallisasank@gmail.com>
2026-07-30 23:33:41 +0530

    Implement stl vector

---
+++ std_vector/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <new>
+
+template <typename T> class Vector {
+	private:
+		size_t size_;
+		size_t capacity_;
+		T* data_;
+
+	public:
+		Vector(size_t capacity)
+			: capacity_(capacity){
+			      	data_ = static_cast<T*>(std::malloc(capacity * sizeof(T)));
+				size_ = 0;	
+			}
+
+		~Vector(){
+			for(int i=0;i<size_;i++){
+				data_[i].~T();
+			}
+			std::free(data_);
+			size_ = 0;
+			capacity_ = 0;
+		}
+
+		void push_back(const T& obj){
+			if(size_ == capacity_){
+				T* temp = static_cast<T*>(std::malloc(((capacity_ == 0) ? 1 : 2*capacity_) * sizeof(T)));
+				for(int i = 0; i < size_;i++){
+					new (temp + i) T(std::move(data_[i]));
+					data_[i].~T();
+				}
+				std::free(data_);
+				data_ = temp;
+				capacity_ = (capacity_ == 0) ? 1 : 2*capacity_; 
+			}
+			new (data_ + size_) T(obj); 
+			size_ += 1;
+		}
+
+		int size() {
+			return size_;
+		}
+
+		T& operator[](std::size_t index){
+			return data_[index];
+		}
+};
+
+int main() {
+	Vector<std::string> v(2);
+	v.push_back("Hello");
+	v.push_back("World");
+	v.push_back("C++");
+	
+	for(int i = 0; i < v.size();i++){
+		std::cout << v[i] << "\n"; 
+	}
+	return 0;
+}
