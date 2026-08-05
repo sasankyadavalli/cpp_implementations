1851414675c27fb86baf96b6dc2d99bd5dceed1d
Sasank Yadavalli <yadavallisasank@gmail.com>
2026-08-05 23:59:24 +0530

    Implement move/copy constructors and assignment

--- std_vector/main.cpp
+++ std_vector/main.cpp
@@ -2,88 +2,219 @@
 #include <new>
 #include <utility>
 
-template <typename T> class Vector {
-	private:
-		std::size_t size_;
-		std::size_t capacity_;
-		T* data_;
-
-		void grow() {
-                         std::size_t new_capacity_ = (capacity_ == 0) ? 1 : 2*capacity_;
-                         T* temp = static_cast<T*>(std::malloc(new_capacity_ * sizeof(T)));
-                         for(std:size_t i = 0; i < size_;i++){
-                                 new (temp + i) T(std::move(data_[i]));
-                                 data_[i].~T();
-                         }
-                         std::free(data_);
-                         data_ = temp;
-                         capacity_ = new_capacity_;
-                }
+template <typename T>
+class Vector
+{
+private:
+	std::size_t size_;
+	std::size_t capacity_;
+	T *data_;
 
 
-	public:
-		// COPY CONSTRUCTOR
-		// MOVE CONSTRUCTOR
-		// 
-
-			Vector(Vector&& other) {
-		    other.data_ = this.data_;
-		    other.capacity_ = this.capacity_;
-		    other.size_ = this.size_;
-		    
-		    this.data_ nullptr;
-		    this.capacity_ = 0;
-		    this.size_ = 0;
+	/*
+		This is currently not exception safe. Should revisit. 
+	*/
+	void grow()
+	{
+		std::size_t new_capacity_ = (capacity_ == 0) ? 1 : 2 * capacity_;
+		T *temp = static_cast<T*>(::operator new(new_capacity_ * sizeof(T)));
+		for (std::size_t i = 0; i < size_; i++)
+		{
+			new (temp + i) T(std::move(data_[i]));
+			data_[i].~T();
 		}
-		Vector(size_t capacity)
-			: capacity_(capacity){
-			      	data_ = static_cast<T*>(std::malloc(capacity * sizeof(T)));
-				size_ = 0;	
+		::operator delete(data_);
+		data_ = temp;
+		capacity_ = new_capacity_;
+	}
+
+public:
+	Vector(const Vector &other)
+		: capacity_(other.capacity_), size_(other.size_)
+	{
+
+		data_ = static_cast<T*>(::operator new(other.capacity_ * sizeof(T)));
+		for (std::size_t i = 0; i < other.size_; i++)
+		{
+			try {
+				new (data_ + i) T(other.data_[i]);
+			}
+			catch(...) {
+				for(std::size_t j = 0; j < i; j++) {
+					data_[j].~T();
+				}
+
+				::operator delete(data_);
+				throw;
+			}
+		}
+	}
+
+	// Move constructor
+	Vector(Vector &&other) noexcept
+		: capacity_(other.capacity_), size_(other.size_), data_(other.data_)
+	{
+
+		other.data_ = nullptr;
+		other.capacity_ = 0;
+		other.size_ = 0;
+
+		std::cout << "Move constructor ended" << std::endl;
+	}
+
+	// Default constructor
+	Vector(size_t capacity)
+		: capacity_(capacity)
+	{
+		data_ = static_cast<T *>(::operator new(capacity * sizeof(T)));
+		size_ = 0;
+	}
+
+	~Vector()
+	{
+		for (std::size_t i = 0; i < size_; i++)
+		{
+			data_[i].~T();
+		}
+		::operator delete(data_);
+	}
+
+	void push_back(const T &obj)
+	{
+		if (size_ == capacity_)
+		{
+			grow();
+		}
+		new (data_ + size_) T(obj);
+		++size_;
+	}
+	
+	template <typename... Args>
+	void emplace_back(Args &&...args)
+	{ 
+		if (size_ == capacity_)
+		{
+			grow();
+		}
+		new (data_ + size_) T(std::forward<Args>(args)...);
+		++size_;
+	}
+
+	std::size_t size() const noexcept
+	{
+		return size_;
+	}
+
+	T &operator[](std::size_t index)
+	{
+		return data_[index];
+	}
+
+	Vector &operator=(const Vector& other)
+	{
+		if (&other == this)
+		{
+			return *this;
+		}
+
+		if (this->capacity_ >= other.capacity_)
+		{
+			for (std::size_t i = 0; i < std::min(this->size_, other.size_); i++)
+			{
+				this->data_[i] = other.data_[i];
 			}
 
-		~Vector(){
-			for(int i=0;i<size_;i++){
-				data_[i].~T();
+			if (this->size_ >= other.size_)
+			{
+				for (std::size_t i = other.size_; i < this->size_; i++)
+				{
+					this->data_[i].~T();
+				}
+
+				this->size_ = other.size_;
 			}
-			std::free(data_);
+			else
+			{
+				for (std::size_t i = this->size_; i < other.size_; i++)
+				{
+					new (data_ + i) T(other.data_[i]);
+					this->size_ += 1;
+				}
+			}
+			return *this;
 		}
 
-		void push_back(const T& obj) {
-			if(size_ == capacity_){
-				grow();
+		T *temp = static_cast<T*>(::operator new(other.capacity_ * sizeof(T)));
+
+		for (std::size_t i = 0; i < other.size_; i++)
+		{
+			try
+			{
+				new (temp + i) T(other.data_[i]);
 			}
-			new (data_ + size_) T(obj); 
-			++size_;
-		}	
-		// template parameter pack
-		template<typename... Args> 
-		void emplace_back(Args&&... args){ //function parameter pack()
-			if(size_ == capacity_) {
-				grow();
+			catch (...)
+			{
+				for (std::size_t j = 0; j < i; ++j)
+				{
+					temp[j].~T();
+				}
+				::operator delete(temp);
+
+				throw;
 			}
-			new (data_ + size_) T(std::forward<Args>(args)...);
-			++size_;
-		
-		}
-		
-		int size() {
-			return size_;
 		}
 
-		T& operator[](std::size_t index){
-			return data_[index];
+		std::swap(this->data_, temp);
+
+		for (std::size_t i = 0; i < size_; i++)
+		{
+			temp[i].~T();
 		}
+
+		capacity_ = other.capacity_;
+		size_ = other.size_;
+
+		::operator delete(temp);
+
+		return *this; //
+	}
+
+	Vector &operator=(Vector&& other) noexcept
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+
+		for (std::size_t i = 0; i < this->size_; i++)
+		{
+			data_[i].~T();
+		}
+
+		::operator delete(this->data_);
+
+		this->data_ = other.data_;
+		this->capacity_ = other.capacity_;
+		this->size_ = other.size_;
+
+		other.data_ = nullptr;
+		other.capacity_ = 0;
+		other.size_ = 0;
+
+		return *this;
+	}
 };
 
-int main() {
-	Vector<std::string> v(2);
-	v.push_back("Hello");
-	v.push_back("World");
-	v.push_back("C++");
-	v.emplace_back("emplace");
-	
-	for(size_t i = 0; i < v.size();i++){
-		std::cout << v[i] << "\n"; 
+int main()
+{
+	Vector<int> v(2);
+	Vector<int> b(10);
+	v.push_back(11);
+	b = v;
+
+	for (size_t i = 0; i < b.size(); i++)
+	{
+		std::cout << b[i] << "\n";
 	}
 	return 0;
 }
